1f8a2841466adade8ec06422efd61565cb221729
ajvernon <a.vernon@outlook.com>
2017-10-10 14:30:09 +0100

    started work on ajax

--- db.php
+++ db.php
@@ -1,3 +1,7 @@
 <?php
 $dbh = new PDO('mysql:host=db702704743.db.1and1.com;dbname=db702704743',' dbo702704743', 'TacticalGenius1-');
+
+function createPoll($db){
+
+}
 ?>
--- script.js
+++ script.js
@@ -2,3 +2,18 @@ window.onerror = function(msg, url, linenumber) {
     alert('Error message: '+msg+'\nURL: '+url+'\nLine Number: '+linenumber);
     return true;
 }
+
+function request(){
+    var httpRequest;
+    function makeRequest() {
+        httpRequest = new XMLHttpRequest();
+    
+        if (!httpRequest) {
+            alert('Giving up :( Cannot create an XMLHTTP instance');
+            return false;
+        }
+        httpRequest.onreadystatechange = alertContents;
+        httpRequest.open('GET', 'test.html');
+        httpRequest.send();
+    }
+}
