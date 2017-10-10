d3de52652ef78788bb0316c64ef1deb55bb3f261
ajvernon <a.vernon@outlook.com>
2017-10-10 15:50:33 +0100

    The PHP should add a table now

--- db.php
+++ db.php
@@ -1,5 +1,14 @@
 <?php
-$dbh = new PDO('mysql:host=db702704743.db.1and1.com;dbname=db702704743',' dbo702704743', 'TacticalGenius1-');
+// Starts the database connection
+$dbh = new PDO('mysql:host=db702704743.db.1and1.com;dbname=db702704743','dbo702704743', 'TacticalGenius1-');
 
-echo $_POST;
+$stmt = $dbh->prepare('CREATE TABLE (:name) (option1 INT, option2 INT, option3 INT);');
+$stmt->bindParam(':name', $name);
+$name = 'butts';
+$stmt->execute();
+echo json_encode($_POST);
+
+// Ends the database connection
+$stmt = null;
+$dbh = null;
 ?>
--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+/*
+ * This function throws an error as an alert, instead of just silently
+ * throwing it into the console.
+ */
 window.onerror = function(msg, url, linenumber) {
     alert('Error message: '+msg+'\nURL: '+url+'\nLine Number: '+linenumber);
     return true;
 }
 
+/*
+ * This block catches the submit button, and throws it to the PHP file.
+ * This could have been done in HTML, but I chose to use JS as I'm familar with it.
+ */
 window.addEventListener("load", function (){
     function makeRequest(inputData) {
         let httpRequest = new XMLHttpRequest();
